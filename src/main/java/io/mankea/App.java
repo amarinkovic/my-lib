2473719128dc00ddc924b655f882a6396b2daa78
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2022-03-23 14:43:13 +0100

    maven archetype created

---
+++ src/main/java/io/mankea/App.java
@@ -0,0 +1,13 @@
+package io.mankea;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
